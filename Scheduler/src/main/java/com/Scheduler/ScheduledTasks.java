e003de4d207284993b7b264df7aa42cea0a92ee5
JuriVitali <S1092588@studenti.univpm.it>
2021-01-11 12:10:13 +0100

    Ultimato lo schedulatore con la documentazione

--- Scheduler/src/main/java/com/Scheduler/ScheduledTasks.java
+++ Scheduler/src/main/java/com/Scheduler/ScheduledTasks.java
@@ -2,14 +2,14 @@ package com.Scheduler;
 
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
-import model.City;
-import model.Record;
-import service.*;
+import com.model.City;
+import com.model.Record;
+import com.service.*;
 import java.util.Vector;
 //import org.springframework.beans.factory.annotation.Autowired;
 
 /**
- * La classe è un componte che implementa uno schedulatore e fa uso dell'interfaccia WeatherService
+ * La classe e' un componente che implementa uno schedulatore e fa uso dell'interfaccia WeatherService
  * 
  * @author Juri Vitali 
  * @author Nicola Sebastianelli 
@@ -22,48 +22,36 @@ public class ScheduledTasks {
 	private WeatherService weatherService = new WeatherServiceImplementation() ;
 	private Vector<Record> lista;
 	/**
-	 * Metodo che viene eseguito ogni ora e che serve per raccogliere informazioni sulle temperature registrate in alcune città su un file
-	 * json che si trova in un account Dropbox. Le città (nome e id) di cui vengono raccolti i dati sono prese da un file contenuto nel 
+	 * Metodo che viene eseguito ogni ora e che serve per raccogliere informazioni sulle temperature registrate in alcune citta' su un file
+	 * json che si trova in un account Dropbox. Le citta' (nome e id) di cui vengono raccolti i dati sono prese da un file contenuto nel 
 	 * medesimo account Dropbox. 
 	 *  <p>
 	 *  In particolare questo metodo:
 	 *  - scarica le informazioni contenute nel file CityList.json (in Dropbox) riguardante le città (questa operazione viene eseguita solo la prima 
 	 *    volta che ile metodo viene eseguito;
-	 *  - esegue una chiamata ad OpenWeather per ogni città e fa il parsing delle informazioni ottenute;
+	 *  - esegue una chiamata ad OpenWeather per ogni citta' e fa il parsing delle informazioni ottenute;
 	 *  - scarica le informazioni contenute nel file Data.json (in Dropbox) riguardanti i dati precedentemente raccolti;
-	 *  - unisce tali informazioni con i dati raccollti mediante le chiamtate avvenute durante questa esecuzione
-	 *  - sovrascrive il file Data.json 
-	 *  Per eseguire tali operazioni il metodo fa uso dell'interfaccia WeatherService
+	 *  - unisce tali informazioni con i dati raccolti mediante le chiamate avvenute durante questa esecuzione;
+	 *  - sovrascrive il file Data.json .
+	 *  Per eseguire tali operazioni il metodo fa uso dell'interfaccia WeatherService.
 	 *  
-	 *  @see WeatherService
+	 *  @see com.service.WeatherService
 	 */
-	/*@Scheduled (fixedRate = 15000) //ogni 15 secondi
+
+	@Scheduled (fixedRate = 3600000) //ogni ora
 	public void ReportData() {
-		if (i==0) {cities=weatherService.getCities((weatherService.download(WeatherService.fileNameCities))); i++;};	
-		String totalData = weatherService.download(WeatherService.fileNameData);
-		for(Record v : weatherService.parse(cities)) totalData += weatherService.produceString(v);
-		weatherService.update(totalData, WeatherService.fileNameData);
-	}    */
-	
-	@Scheduled (fixedRate = 15000) //ogni 15 secondi
-	public void ReportData() {
-		i++;
 		String totalData = new String();
-		if (i==1) {
-		String s=weatherService.download(WeatherService.fileNameCities);
-		cities=weatherService.getCities(s);
-		totalData += '[';
-		 }
-		totalData += weatherService.download(WeatherService.fileNameData);
-		//if (totalData.isEmpty()) totalData = weatherService.download(WeatherService.fileNameData);
-		//else totalData += weatherService.download(WeatherService.fileNameData);
+		if (i==0) {
+			cities=weatherService.getCities(weatherService.download(WeatherService.fileNameCities)); 
+			totalData += '['; 
+			i++;
+		}
+		totalData += weatherService.download(WeatherService.fileNameData) + "\n\n";
 		lista=weatherService.parse(cities);
-		for(Record v : lista) {
+		for(Record v : lista) 
 			if (i==1 && v==lista.get(0)) totalData += weatherService.produceString(v); 
 			else totalData += " , "+weatherService.produceString(v);
-		}
 		totalData += ']';
-		System.out.println(totalData);
 		weatherService.update(totalData, WeatherService.fileNameData);
 	}  
 	
--- Scheduler/src/main/java/com/Scheduler/SchedulerApplication.java
+++ Scheduler/src/main/java/com/Scheduler/SchedulerApplication.java
@@ -3,10 +3,12 @@ package com.Scheduler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import com.service.WeatherService;
+import com.service.WeatherServiceImplementation;
 
 
 /**
- * Classe che contiene solamente il main, il cui scopo è quello di avvviare l'unico componente consistente nella classe Scheduled Tasks
+ * Classe che contiene solamente il main e ha lo scopo di avvia lo scheduler e puo' resettare il file contenente i dati sulle temperature
  * 
  * @author Juri Vitali
  * @author Nicola Sebstianelli
@@ -16,10 +18,18 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class SchedulerApplication {
 
 	/**
-	 * il cui scopo è quello di avvviare l'unico componente, consistente nella classe Scheduled Tasks
+	 * Il main avvia lo scheduler. Prima di farlo cancella tutti i dati dal file contenente i dati sulle temperature se gli si passa come 
+	 * parametro (da riga di comando) la stringa reset
+	 * 
+	 * @param args questo parametro è opzionale. Se viene passata la stringa "reset" cancella tutti i dati dal file contenente  i dati sulle temperature
+	 *            
 	 */
 	public static void main(String[] args) {
-		SpringApplication.run(SchedulerApplication.class);
+		if (args.length!=0 && args[0] == "reset") {
+			WeatherService weatherService = new WeatherServiceImplementation() ;
+			weatherService.update("",WeatherService.fileNameData);
+		}
+		SpringApplication.run(SchedulerApplication.class, args);
 	}
 
 }
